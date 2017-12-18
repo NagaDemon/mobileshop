68b3229d3743a56c3b4af0e029660cf61391d218
Tuan Hoang <tuanhm2911@gmail.com>
2017-12-18 09:08:55 +0700

    update

--- chucnang/timkiem/danhsachtimkiem.php
+++ chucnang/timkiem/danhsachtimkiem.php
@@ -57,7 +57,29 @@ if($pageNum = 1) {
 }
 
 // Tìm kiếm từ khóa
-
+?>
+<div class="prd-block">
+    <h2>kết quả tìm được với từ khóa <span class="skeyword">"<?php echo $stext;?>"</span></h2>
+    <div class="pr-list">
+        <?php
+        $i = 1;
+        while($row = mysql_fetch_array($query)){
+        ?>
+        <div class="prd-item">
+            <a href="#"><img width="80" height="144" src="quantri/anh/<?php echo $row['anh_sp'];?>" /></a>
+            <h3><a href="#"><?php echo $row['ten_sp'];?></a></h3>
+            <p>Bảo hành: <?php echo $row['bao_hanh'];?></p>
+            <p>Tình trạng: <?php echo $row['tinh_trang'];?></p>
+            <p class="price"><span>Giá: <?php echo $row['gia_sp'];?> VNĐ</span></p>
+        </div>
+        <?php
+            if($i%3==0){
+                echo '<div class="clear"></div>';
+            }
+        }
+        ?>        
+    </div>
+</div>
 
 <div class="clear"></div>
 <div id="pagination"><?php echo $listPages;?></div>
