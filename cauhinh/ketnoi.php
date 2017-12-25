c3b0ec1ef40e2da6a589ee6136dadef8af75a826
Tin Tran <tranquangtin710@gmail.com>
2017-12-25 23:39:50 +0700

    update

--- cauhinh/ketnoi.php
+++ cauhinh/ketnoi.php
@@ -5,5 +5,5 @@
 	$dbName = 'mobileshop';
 	$dbConnect = mysql_connect($dbHost, $dbUSer, $dbPass);
 	$dbSelected = mysql_select_db($dbName, $dbConnect);
-	$dbSetLang = mysql_query("SET NAMES utf8");
+	$dbSetLang = mysql_query("SET NAMES utf8"); 
 ?>
